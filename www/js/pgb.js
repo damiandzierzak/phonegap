b7cf3ef51c21d39eddd981485e2101196c495ddd
s-116-27 <s-116-27@gumiforest.ae.krakow.pl>
2017-03-04 17:33:51 +0100

    add motion

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -32,8 +32,15 @@ function checkConnection() {
     alert('Connection type: ' + states[networkState]);
 }
 
-document.addEventListener("deviceready", onDeviceReady, false);
-
-function onDeviceReady() {
-	$('#acctext').text(navigator.accelerometer);
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
 }
+
+function onError() {
+    alert('onError!');
+}
+
+navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
