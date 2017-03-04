38217cee8499d8c323f3feb2dc22f4a214afde97
s-116-27 <s-116-27@gumiforest.ae.krakow.pl>
2017-03-04 17:41:58 +0100

    add contacts

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -5,6 +5,8 @@ function init() {
 function onDeviceReady() {
 	navigator.notification.beep(1);
 	navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+	
+
 }
 var options = { frequency: 3000 };
 
@@ -15,6 +17,10 @@ function pesonalData() {
 		'Group number: hfhrwu23r834\n' +
 		'Field of study: IT';
 	navigator.notification.alert(data);
+}
+
+function contacts(){
+	navigator.contacts.find(filter, onSuccessCon, onErrorCon, options);
 }
 
 function checkConnection() {
@@ -40,7 +46,31 @@ function onSuccess(acceleration) {
           'Timestamp: '      + acceleration.timestamp + '\n');
 }
 
-function onError() {
+function onErrorCon() {
     alert('onError!');
 }
 
+function onSuccessCon(contacts) {
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < contacts[i].addresses.length; j++) {
+            alert("Pref: "         + contacts[i].addresses[j].pref          + "\n" +
+                "Type: "           + contacts[i].addresses[j].type          + "\n" +
+                "Formatted: "      + contacts[i].addresses[j].formatted     + "\n" +
+                "Street Address: " + contacts[i].addresses[j].streetAddress + "\n" +
+                "Locality: "       + contacts[i].addresses[j].locality      + "\n" +
+                "Region: "         + contacts[i].addresses[j].region        + "\n" +
+                "Postal Code: "    + contacts[i].addresses[j].postalCode    + "\n" +
+                "Country: "        + contacts[i].addresses[j].country);
+        }
+    }
+};
+ 
+function onError(contactError) {
+    alert('onError!');
+};
+ 
+// find all contacts 
+var options = new ContactFindOptions();
+options.filter = "";
+options.multiple = true;
+var filter = ["displayName", "addresses"];
